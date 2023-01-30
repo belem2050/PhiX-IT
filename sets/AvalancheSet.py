2863caea2faf561f3d9b8a708ebe63a2c252896e
kosmaprzyjemski <kosmaprzyjemski@gmail.com>
2023-01-31 00:13:11 +0100

    replaced snow depth by humidity

--- sets/AvalancheSet.py
+++ sets/AvalancheSet.py
@@ -1,4 +1,4 @@
-from API.openWeatherMapAPI import getTemperature
+from API.openWeatherMapAPI import getTemperature, getHumidity
 
 
 class AvalancheSet():
@@ -7,6 +7,17 @@ class AvalancheSet():
 
     def getAvalancheRisk(self, city):
         temperature = getTemperature(city)
+        humidity = getHumidity(city)
+        # we use humidity cuz no snow depth information are easily available from APIs
+        
+        if temperature < -15 and humidity < 20:
+            return "No avalanche risk"
+        elif temperature > 0 and humidity > 80:
+            return "Major avalanche risk"
+        else:
+            return "Minor avalanche risk"
+
+        ''' # ideally we would use snow depth for avalanche prediction ...
         snow_depth = getSnowDepth(city)
 
         if snow_depth < 0.2:
@@ -14,4 +25,4 @@ class AvalancheSet():
         elif snow_depth > 0.5 and temperature > 0:
             return "Major avalanche risk"
         else:
-            return "Minor avalanche risk"
+            return "Minor avalanche risk"'''
