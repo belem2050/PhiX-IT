b38463d34b7403f0560b2421ae657c4caad45bd7
Moumouni BELEM <90107382+BELEMMoumouni@users.noreply.github.com>
2023-01-30 22:43:14 +0100

    weather2

--- API/weather2api.py
+++ API/weather2api.py
@@ -3,16 +3,15 @@ import json # encode et décode JSON
 
 Location = input('post code of city : ')
 Location = "fr."+Location
-APP_KEY="779e6f7f895bea2f15eba6b89cfbb7f5"
+APP_KEY="9d0804bd0ac1549985268ca3b72730a0"
 APP_ID = "a6165965"
 LocalWeatherType = "current"
 
 
-x = requests.get(f'http://api.weatherunlocked.com/api/{LocalWeatherType}/{Location}?app_id={APP_ID}&app_key={APP_KEY}') # FAKE JSON file
+x = requests.get(f'http://api.weatherunlocked.com/api/{LocalWeatherType}/{Location}?app_id={APP_ID}&app_key={APP_KEY}') # 
 
 
 print(x.text)
 
 answer = json.loads(x.text)
-print(answer["wx_desc"])
-print(answer["wx_desc"])
+
